30adc2aea089ccddf9b08397b9619b901f4834c0
Nicholas Jackson <jackson.nic@gmail.com>
2019-02-20 10:20:08 +0000

    Added health check for cache

--- handlers/health.go
+++ handlers/health.go
@@ -1,22 +1,28 @@
 package handlers
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/emojify-app/api/emojify"
 	"github.com/emojify-app/api/logging"
+	"github.com/emojify-app/cache/protos/cache"
+	"github.com/golang/protobuf/ptypes/wrappers"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // Health is a HTTP handler for serving health requests
 type Health struct {
 	logger logging.Logger
 	em     emojify.Emojify
+	cc     cache.CacheClient
 }
 
 // NewHealth returns a new instance of the Health handler
-func NewHealth(l logging.Logger, em emojify.Emojify) *Health {
-	return &Health{l, em}
+func NewHealth(l logging.Logger, em emojify.Emojify, cc cache.CacheClient) *Health {
+	return &Health{l, em, cc}
 }
 
 // ServeHTTP implements the http.Handler interface
@@ -27,6 +33,13 @@ func (h *Health) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	// check facebox health
 	info, err := h.em.Health()
 	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// check cache health
+	_, err = h.cc.Get(context.Background(), &wrappers.StringValue{Value: "notexist"})
+	if s := status.Convert(err); s != nil && s.Code() != codes.NotFound {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
