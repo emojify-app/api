332c9acc15e37e8f62cc9955d623fd5e5b0f79c9
Nicholas Jackson <jackson.nic@gmail.com>
2019-03-02 14:42:26 +0000

    Fix problem with logging

--- logging/logger.go
+++ logging/logger.go
@@ -107,7 +107,12 @@ func (l *LoggerImpl) CacheHandlerCalled(r *http.Request) Finished {
 
 	return func(status int, err error) {
 		l.s.Timing(statsPrefix+"cache.called", time.Now().Sub(st), getStatusTags(status), 1)
-		l.l.Error("Cache handler finished", "status", status)
+		if err != nil {
+			l.l.Error("Cache handler finished", "status", status, "error", err)
+			return
+		}
+
+		l.l.Debug("Cache handler finished", "status", status)
 	}
 }
 
