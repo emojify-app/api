e79b6c5a2025e3fa06eef9f9b39c1667a912be06
Nicholas Jackson <jackson.nic@gmail.com>
2019-04-25 09:31:51 +0000

    update to use environment variables

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/nicholasjackson/env"
 	"google.golang.org/grpc"
 
 	"github.com/emojify-app/api/handlers"
@@ -29,36 +30,48 @@ func init() {
 
 var version = "dev"
 
-var bindAddress = flag.String("bind-address", "localhost:9090", "Bind address for the server defaults to localhost:9090")
-var path = flag.String("path", "/", "Path to mount API, defaults to /")
+var bindAddress = env.String("BIND_ADDRESS", false, "localhost:9090", "Bind address for the server defaults to localhost:9090")
+var path = env.String("PATH", false, "/", "Path to mount API, defaults to /")
 
 // authentication flags
-var redisLocation = flag.String("redis-location", "localhost:1234", "Location for the redis server")
-var redisPassword = flag.String("redis-password", "", "Password for redis server")
-var allowedOrigin = flag.String("allow-origin", "*", "CORS origin")
-var authNServer = flag.String("authn-server", "http://localhost:3000", "AuthN server location")
-var audience = flag.String("authn-audience", "emojify", "AuthN audience")
-var disableAuth = flag.Bool("authn-disable", false, "Disable authn integration")
+var redisLocation = env.String("REDIS_LOCATION", false, "localhost:1234", "Location for the redis server")
+var redisPassword = env.String("REDIS_PASSWORD", false, "", "Password for redis server")
+var allowedOrigin = env.String("ALLOW_ORIGIN", false, "*", "CORS origin")
+var authNServer = env.String("AUTHN_SERVER", false, "http://localhost:3000", "AuthN server location")
+var audience = env.String("AUTHN_AUDIENCE", false, "emojify", "AuthN audience")
+var disableAuth = env.Bool("AUTHN_DISABLE", false, false, "Disable authn integration")
 
 // external service flags
-var statsDServer = flag.String("statsd-server", "localhost:8125", "StatsD server location")
-var emojifyAddress = flag.String("emojify-address", "localhost", "Address for the Emojify service")
-var cacheAddress = flag.String("cache-address", "localhost", "Address for the Cache service")
-var paymentGatewayURI = flag.String("payment-address", "localhost", "Address for the Payment gateway service")
+var statsDServer = env.String("STATSD_SERVER", false, "localhost:8125", "StatsD server location")
+var emojifyAddress = env.String("EMOJIFY_ADDRESS", false, "localhost", "Address for the Emojify service")
+var cacheAddress = env.String("CACHE_ADDRESS", false, "localhost", "Address for the Cache service")
+var paymentGatewayURI = env.String("PAYMENT_ADDRESS", false, "localhost", "Address for the Payment gateway service")
 
 // logging settings
-var logFormat = flag.String("log-format", "text", "Log output format [text,json]")
-var logLevel = flag.String("log-level", "info", "Log output level [trace,info,debug,warn,error]")
+var logFormat = env.String("LOG_FORMAT", false, "text", "Log output format [text,json]")
+var logLevel = env.String("LOG_LEVEL", false, "info", "Log output level [trace,info,debug,warn,error]")
 
 // performance testing flags
 // these flags allow the user to inject faults into the service for testing purposes
-var cacheErrorRate = flag.Float64("cache-error-rate", 0.0, "Percentage where cache handler will report an error")
-var cacheErrorType = flag.String("cache-error-type", "http_error", "Type of error [http_error, delay]")
-var cacheErrorCode = flag.Int("cache-error-code", http.StatusInternalServerError, "Error code to return on error")
-var cacheErrorDelay = flag.Duration("cache-error-delay", 0*time.Second, "Error delay [1s,100ms]")
+var cacheErrorRate = env.Float64("CACHE_ERROR_RATE", false, 0.0, "Percentage where cache handler will report an error")
+var cacheErrorType = env.String("CACHE_ERROR_TYPE", false, "http_error", "Type of error [http_error, delay]")
+var cacheErrorCode = env.Int("CACHE_ERROR_CODE", false, http.StatusInternalServerError, "Error code to return on error")
+var cacheErrorDelay = env.Duration("CACHE_ERROR_DELAY", false, 0*time.Second, "Error delay [1s,100ms]")
+
+var help = flag.Bool("help", false, "--help to show help")
 
 func main() {
 	flag.Parse()
+	env.Parse()
+
+	// if the help flag is passed show configuration options
+	if *help == true {
+		fmt.Println("Emojify API version:", version)
+		fmt.Println("Configuration values are set using environment variables, for info please see the following list:")
+		fmt.Println("")
+		fmt.Println(env.Help())
+		os.Exit(0)
+	}
 
 	logger, err := logging.New("api", version, *statsDServer, *logLevel, *logFormat)
 	if err != nil {
